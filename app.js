3bd11de2cb68edfa83a9e8dd8060e74af33d67d9
Iury Figueiredo <iury.figueiredocampos@gmail.com>
2025-03-14 13:51:32 -0300

    Concluída lógica do programa

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+function adicionarAmigo() {
+    let campoNovoAmigo = document.querySelector ('input');
+    let novoAmigo = campoNovoAmigo.value;
+    if (novoAmigo == []) {
+        alert ('Por favor, insira um nome.');
+        return;
+    } else {
+        amigos.push (novoAmigo);
+        campoNovoAmigo.value = "";
+        campoNovoAmigo.focus ();
+        console.log (amigos);
+        exibirListaAmigos();
+    }
+}
+function exibirListaAmigos() {
+    let listaAmigos = document.getElementById ('listaAmigos');
+    listaAmigos.innerHTML = '';
+    for (let index = 0; index < amigos.length; index++) {
+        let amigoNaTela = document.createElement('li');
+        amigoNaTela.textContent = amigos[index];
+        listaAmigos.appendChild(amigoNaTela);
+    }
+}
+function sortearAmigo(){
+    if (amigos.length == 0) {
+        alert ('Por favor, insira um nome.');
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random()*amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    let resultadoNaTela = document.getElementById('resultado');
+    resultadoNaTela.innerHTML = `A pessoa sorteada foi ${amigoSorteado}`;
+    let limparListaAmigos = document.getElementById ('listaAmigos');
+    limparListaAmigos.innerHTML = '';
+    amigos = [];
+}
